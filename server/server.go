5e15bdb8480b2410f1af19179a5f92420acabbb9
ray <ray@uregina.ca>
2010-11-08 14:01:34 -0600

    got the ws.Read/Write working

--- server/server.go
+++ server/server.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"http"
-	"io"
+	//"io"
+	//"os"
 	"websocket"
 )
 
 // Echo the data received on the Web Socket.
-func EchoServer(ws *websocket.Conn) {
-	io.Copy(ws, ws);
+func GetPos(ws *websocket.Conn) {
+	var msg = make([]byte, 10)
+
+	for n, err := ws.Read(msg); err == nil; ws.Write(msg[0:n]) {
+		n, err = ws.Read(msg)
+	}
+
+	ws.Close();
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(EchoServer));
+	http.Handle("/getpos", websocket.Handler(GetPos));
 	err := http.ListenAndServe(":12345", nil);
 	if err != nil {
 		panic("ListenAndServe: " + err.String())
