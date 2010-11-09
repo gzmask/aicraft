d15b30fb56c6a69bf11394158e9bbb7e9d34911e
Ray Lei <gzmask@Ray-Leis-MacBook-Pro.local>
2010-11-08 22:27:49 -0600

    taskes rearrange

---
+++ server/make6.sh
@@ -0,0 +1,6 @@
+6g player.go
+6g ai.go
+6g socketserver.go
+6g main.go
+6l main.6
+./6.out
--- server/socketserver.go
+++ server/socketserver.go
@@ -3,7 +3,22 @@ package socketserver
 import (
 	"http"
 	"websocket"
+	"./player"
+	"./ai"
 )
+
+var p1 player.Player
+var p2 player.Player
+var a1 ai.Ai
+var a2 ai.Ai
+
+func init() {
+	p1.X, p1.Y = 0.0, 0.0
+	p2.X, p2.Y = 5.0, 5.0
+	a1.X, a1.Y = 1.0, 1.0
+	a2.X, a2.Y = 4.0, 4.0
+}
+
 
 //websocket service telling all units' positions 
 func GetPos(ws *websocket.Conn) {
