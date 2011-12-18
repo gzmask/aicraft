7ec3418e366ba5fbae7124b3962811fe68813ae1
gzmask <gzmask@hotmail.com>
2011-12-18 14:15:17 -0600

    add eclipse project

--- client/js/ai.js
+++ client/js/ai.js
@@ -6,6 +6,6 @@ function Ai(x,y) {
 	this.draw = function(ctx) {
 		ctx.fillStyle = "rgb(200,200,0)";  
 		ctx.fillRect (this.x, this.y, 10, 10);  
-	}
+	};
 
 }
--- client/js/player.js
+++ client/js/player.js
@@ -6,6 +6,6 @@ function Player(x,y) {
 	this.draw = function(ctx) {
 		ctx.fillStyle = "rgb(200,0,0)";  
 		ctx.fillRect (this.x, this.y, 10, 10);  
-	}
+	};
 
 }
