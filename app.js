14747445b026bead394bab58d76983ea5ea4ed6a
gzmask <gzmask@gmail.com>
2011-12-20 16:33:17 -0600

    app.js added socket.io

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //entry point
+(function() {
+  var app, express, io;
+  express = require('express');
+  io = require('socket.io');
+
+  app = express.createServer();
+  app.set('views',__dirname+'/views');
+  app.set('view engine', 'ejs');
+
+  app.use(express.static(__dirname + '/public'));
+  app.get('/', function(request, response) {
+    return response.render('index');
+  });
+  app.listen(3000);
+  io = io.listen(app);
+  console.log("Express server started on port %s", app.address().port);
+  
+  io.sockets.on('connection', function (socket) {
+	  socket.emit('news', { hello: 'world' });
+	  socket.on('my other event', function (data) {
+	    console.log(data);
+	  });
+	});
+  
+}).call(this);
