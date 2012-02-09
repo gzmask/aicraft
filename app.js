89709e0437e7b1bcd41194848ab817b07edca6c5
gzmask <gzmask@gmail.com>
2012-02-09 14:42:19 -0600

    fixed ammo so that socket.io works

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
 //entry point
 (function() {
-  var app, express;
-  express = require('express');
-  //var io = require('socket.io');
+	var app, express, nowjs;
+	express = require('express');
+	nowjs = require("now");
 
-  //web server
-  app = express.createServer();
-  app.set('views',__dirname+'/views');
-  app.set('view engine', 'ejs');
+	//web server
+	app = express.createServer();
+	app.set('views',__dirname+'/views');
+	app.set('view engine', 'ejs');
 
-  app.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 
-  app.get('/', function(request, response) {
-    return response.render('index');
-  });
+	app.get('/', function(request, response) {
+		return response.render('index');
+	});
 
-  app.get('/game', function(request, response) {
-    return response.render('game/index');
-  });
+	app.get('/game', function(request, response) {
+		return response.render('game/index');
+	});
 
-  app.get('/mp_demo', function(request, response) {
-    return response.render('game/mp_demo');
-  });
-
-  app.listen(3003);
-  console.log("Express server started on port %s", app.address().port);
-  
-  //game server
-	var nowjs = require("now");
+	app.listen(3003);
+	console.log("Express server started on port %s", app.address().port);
+	  
+	//game server
 	var everyone = nowjs.initialize(app);
 
+	//THREE = require('./public/js/three.js');
+	Ammo = require('./public/js/ammo.js');
 	everyone.now.logStuff = function(msg){
 		console.log(msg);
-	}
-  //io = io.listen(app);
-  //io.sockets.on('connection', function (socket) {
-//	  socket.emit('news', { hello: 'world' });
-//	  socket.on('my other event', function (data) {
-//	    console.log(data);
-//	  });
-//	});
+	};
   
 }).call(this);
