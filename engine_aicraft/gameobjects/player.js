053687bd1a4f250b4d5c47be2238eb765105e30f
gzmask <gzmask@gmail.com>
2012-02-14 23:08:39 -0600

    minor

--- engine_aicraft/gameobjects/player.js
+++ engine_aicraft/gameobjects/player.js
@@ -1,12 +1,6 @@
 AICRAFT.Player = function (x,y,z,qx,qy,qz,qw) {
 	this.maxSpeed = 20;
 	this.acceleration = 4;
-	//first player joins the game isFirst === true
-	this.isFirst = false;
-	//last player joins the game isLast === true, then the game starts
-	this.isLast = false;
-	//other players will be isMiddle === ture.
-	this.isMiddle = false;
 	AICRAFT.GameObject.call(this,x,y,z,qx,qy,qz,qw);
 };
 
